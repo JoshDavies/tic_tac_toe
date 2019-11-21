14b3323b82e425ffcfe23722bd057009fa7e04b7
Josh Davies <jdaviescreative@gmail.com>
2019-11-21 11:15:32 +0000

    first turn, player move can take any field

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Game
+  def initialize
+    @fields = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
 
-  def player_move(field)
-    ['X', 2, 3, 4, 5, 6, 7, 8, 9]
+  attr_reader :fields
+
+  def player_move(move)
+    @fields[move - 1] = 'X'
   end
 end
+
+
+
+game = Game.new
+game.player_move(5)
+p game.fields
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,10 +4,15 @@ require 'game'
 
 describe Game do
   let(:game) { Game.new }
-  
+
   describe '#player_move' do
     it 'allows player1 to enter their move' do
-      expect(game.player_move(1)).to eq(['X', 2, 3, 4, 5, 6, 7, 8, 9])
+      game.player_move(1)
+      expect(game.fields).to eq(['X', 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+    it 'allows player1 to enter their move in any field' do
+      game.player_move(4)
+      expect(game.fields).to eq([1, 2, 3, 'X', 5, 6, 7, 8, 9])
     end
   end
 end
